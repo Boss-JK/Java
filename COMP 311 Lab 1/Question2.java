76c566b17705a3a39570842ae8c23db9adb9ac78
Boss Kerobale <bosskerobale15@gmail.com>
2026-08-26 17:29:27 +0200

    Question 2: Primitive Datatypes

---
+++ COMP 311 Lab 1/Question2.java
@@ -0,0 +1,34 @@
+/*
+*Student Name: Bosa Jeremiah Kerobale
+*Student ID: 24019732
+*Question 2
+*/
+public class Question2 {
+    public static void main(String[] args) {
+        //Intialisation and Declaration of variables
+        byte byteValue = 100;
+        short shortValue = 20000;
+        int integerValue = 350000;
+        long longValue = 9000000000L;
+        float floatValue = 3.14f;
+        double doubleValue = 3.14159265;
+        char firstCharacter = 'J';
+        char secondCharacter = 'B';
+        boolean firstBooleanValue = true;
+        boolean secondBooleanValue = false;
+
+
+        //Display the primitive data types
+        System.out.println("byte: " + byteValue);
+        System.out.println("short: " + shortValue);
+        System.out.println("int: " + integerValue);
+        System.out.println("long: " + longValue);
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+        System.out.println("char: " + firstCharacter);
+        System.out.println("char: " + secondCharacter);
+        System.out.println("boolean: " + firstBooleanValue);
+        System.out.println("boolean: " + secondBooleanValue);
+
+    }
+}

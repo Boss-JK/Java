64824a31955411a1552f774a716e284fcfbeab34
Boss Kerobale <bosskerobale15@gmail.com>
2026-08-26 16:48:45 +0200

    Question 1: Personal Profile

---
+++ COMP 311 Lab 1/Question1.java
@@ -0,0 +1,23 @@
+/*
+*StudentID: 24019732
+*Student Names: Bosa Jeremiah Kerobale
+*Question 1
+*/
+
+public class Question1 {
+    public static void main(String[] args){
+        //Intialisation and Declaration of variables
+        String fullName = "Bosa Jeremiah Kerobale";
+        int studentID = 24019732;
+        int age = 19;
+        double gpa = 4.1;
+        boolean fullTime = true;
+
+        // Display personal profit
+        System.out.println("Name: " + fullName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Full-time: " + fullTime);
+    }
+}

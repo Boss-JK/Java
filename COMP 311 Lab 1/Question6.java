83ba080a9a8daeffd7dd07aaff7fe9aea16346b7
Boss Kerobale <bosskerobale15@gmail.com>
2026-08-28 11:22:23 +0200

    Question 7: Temperature Convertion

--- COMP 311 Lab 1/Question6.java
+++ COMP 311 Lab 1/Question6.java
@@ -7,16 +7,17 @@ import java.util.Scanner;
 public class Question6 {
     public static void main (String[] args){
         Scanner scan = new Scanner (System.in);
-        // Ask user to enter three scores
-        System.out.print("Enter three test scores by spacing them: ");
-        //Reading the user's three test scores
+        // Ask user to enter four scores
+        System.out.print("Enter four test scores by spacing them: ");
+        //Reading the user's four test scores
         int firstScore = scan.nextInt();
         int secondScore = scan.nextInt();
-        int thirdScore = scan.nextInt();        
+        int thirdScore = scan.nextInt(); 
+        int fourthScore = scan.nextInt();
         scan.close();
 
         //Calculating the average of the user's three three test scores
-        double average = (firstScore + secondScore + thirdScore) / 3.0;
+        double average = (firstScore + secondScore + thirdScore + fourthScore) / 4.0;
         // Displaying the average
         System.out.println("Average: " + average);
     }
---
+++ COMP 311 Lab 1/Question7.java
@@ -0,0 +1,22 @@
+/*
+* Student Name: Bosa Jeremiah Kerobale
+* Student ID: 24019732
+* Question 7
+*/
+import java.util.Scanner;
+public class Question7 {
+    public static void main (String[] args){
+        Scanner scan = new Scanner (System.in);
+        // Ask user to enter the temperature
+        System.out.print("Enter the temperature in Degrees Celsius: ");
+        // Reading the user's temperature
+        double degreesCelsius = scan.nextDouble();
+        scan.close();
+
+        //Converting the temperature in degrees Celsius to Fahrenheit
+        double degreesFahrenheit = (degreesCelsius * (9.0/5.0)) + 32;
+
+        //Displaying the temperature in degrees celsius and fahrenheit
+        System.out.println(degreesCelsius + " degrees celsius in degrees fahrenheit is " + degreesFahrenheit);
+    }
+}

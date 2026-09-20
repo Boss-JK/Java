6adab6f9768944977b7239e3b9d24adfb5ad832a
Boss Kerobale <bosskerobale15@gmail.com>
2026-09-20 20:35:43 +0200

    Question 1: Creating a class

---
+++ COMP 311 Lab 5/Book.java
@@ -0,0 +1,11 @@
+/*
+* Student Name: Bosa Jeremiah Kerobale
+* Student ID: 24019732
+* Question 1
+*/
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+}

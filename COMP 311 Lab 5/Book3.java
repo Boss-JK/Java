5964dead0d7d89f1dfa4081830b1da95d6287239
Boss Kerobale <bosskerobale15@gmail.com>
2026-09-20 22:22:34 +0200

    PAIN!!!!!

---
+++ COMP 311 Lab 5/Book3.java
@@ -0,0 +1,22 @@
+public class Book3 {
+       private String title;
+   private String author;
+   private double price;
+
+   public void getterauthor(String author) {
+        this.author = author;
+   }
+   public void gettertitle(String title) {
+        this.title = title;
+   }
+   public void getterprice(double price) {
+        this.price = price;
+   }
+
+   public void setter(String author, String title, double price) {
+        this.author = author;
+        this.title = title;
+        this.price = price;
+   }
+
+}
